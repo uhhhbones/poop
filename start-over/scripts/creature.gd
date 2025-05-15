09a7a396b4cd4df37fa7d90f5fe729bb37a67093
uhhhbones <chrisv2.0b@gmail.com>
2025-05-15 03:28:24 -0700

    update 2
    added sprites and updated new and existing features

---
+++ start-over/scripts/creature.gd
@@ -0,0 +1,11 @@
+extends CharacterBody2D
+
+
+const SPEED = 10.0
+
+
+func _physics_process(delta: float) -> void:
+	var direction = Input.get_vector("move_left","move_right","move_up","move_down") 
+	velocity += direction * SPEED
+	move_and_slide()
+	
--- start-over/scripts/game.gd
+++ start-over/scripts/game.gd
@@ -2,7 +2,12 @@ class_name Game
 extends Node
 
 var poop : int 
-var funny_label = "[tornado] %s poops" % str(poop)
+var funny_label = "[pulse] %s poops" % str(poop)
+func spawn_creature():
+	var new_creature = preload("res://scenes/creature.tscn").instantiate()
+	new_creature.global_position = $cave.global_position
+	add_child(new_creature)
+
 
 
 func change_label():
@@ -15,9 +20,10 @@ func _physics_process(_delta: float) -> void:
 
 func _on_animation_player_poop() -> void:
 	poop += 1
-	funny_label = "[tornado] %s poops" % str(poop)
+	funny_label = "[pulse] %s poops" % str(poop)
 	print("pooped %s times" % str(poop) )
 
 
 func _on_button_pressed() -> void:
 	print("creature spawned")
+	spawn_creature()
